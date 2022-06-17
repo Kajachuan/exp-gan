05aa77eb47be06a1a5ee4a3c9066c9b999f14227
Kevin Cajachuán <kevincajachuan@hotmail.com>
2022-06-16 21:16:16 -0300

    Dataset y script de entrenamiento

--- dataset.py
+++ dataset.py
@@ -9,14 +9,15 @@ class MUSDB18Dataset(Dataset):
     """
     Dataset MUSDB18
     """
-    def __init__(self, base_path: str, subset: str, split: str, target: str, duration: Optional[float],
-                 samples: int = 1, random: bool = False, partitions: int = 1) -> None:
+    def __init__(self, base_path: str, subset: str, split: str, 
+                 duration: Optional[float], nfft: int, samples: int = 1, 
+                 random: bool = False, partitions: int = 1) -> None:
         """
         base_path -- Ruta del dataset
         subset -- Nombre del conjunto: 'train' o 'test'
         split -- División del entrenamiento: 'train' o 'valid' cuando subset='train'
-        target -- Instrumento que se va a separar 'vocals', 'drums', 'bass' o 'vocal'
         duration -- Duración de cada canción en segundos
+        nfft -- Cantidad de puntos de la fft
         samples -- Cantidad de muestras de cada cancion
         random -- True si se van a mezclar las canciones de forma aleatoria
         partitions -- Cantidad de particiones de las canciones de validación
@@ -24,23 +25,22 @@ class MUSDB18Dataset(Dataset):
         super(MUSDB18Dataset, self).__init__()
         self.sample_rate = 44100 # Por ahora no se usa
         self.split = split
-        self.target = target
         self.duration = duration
+        self.nfft = nfft
         self.samples = samples
         self.random = random
         self.partitions = partitions
+        self.window = torch.hann_window(nfft)
+        self.stems = ['vocals', 'drums', 'bass', 'other']
         self.mus = musdb.DB(root=base_path, subsets=subset, split=split)
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
         if self.split == 'train' and self.duration:
             sources = []
-            target_idx = None
             track = self.mus[index // self.samples]
+            orig_mix = torch.zeros(2, int(self.sample_rate * self.duration))
 
-            for idx, source in enumerate(self.mus.setup['sources']):
-                if source == self.target:
-                    target_idx = idx
-
+            for source in self.stems:
                 if self.random:
                     track = random.choice(self.mus)
 
@@ -52,11 +52,14 @@ class MUSDB18Dataset(Dataset):
                 if random.random() < 0.5:
                     audio = np.flipud(audio)
                 audio = torch.as_tensor(audio.copy(), dtype=torch.float32)
-                sources.append(audio)
+                orig_mix += audio
+                stft = torch.stft(audio, n_fft=self.nfft, window=self.window,
+                                  onesided=True, return_complex=True)
+                sources.append(stft)
 
-            stems = torch.stack(sources, dim=0)
-            x = stems.sum(0)
-            y = stems[target_idx]
+            x = torch.stft(orig_mix, n_fft=self.nfft, window=self.window,
+                           onesided=True, return_complex=True)
+            y = torch.stack(sources, dim=0)
 
         # Validación y Test
         else:
@@ -69,9 +72,17 @@ class MUSDB18Dataset(Dataset):
             else:
                 track.chunk_duration = chunk
 
-            x = torch.as_tensor(track.audio.T, dtype=torch.float32)
-            y = torch.as_tensor(track.targets[self.target].audio.T, dtype=torch.float32)
-        return x, y
+            sources = []
+            for source in self.stems:
+                audio = track.sources[source].audio.T
+                stft = torch.stft(audio, n_fft=self.nfft, window=self.window,
+                                  onesided=True, return_complex=True)
+                sources.append(stft)
+
+            x = torch.stft(track.audio.T, n_fft=self.nfft, window=self.window,
+                           onesided=True, return_complex=True)
+            y = torch.stack(sources, dim=0)
+        return (x.real(), x.imag()), (y.real(), y.imag())
 
     def __len__(self) -> int:
         return len(self.mus) * self.samples * self.partitions
